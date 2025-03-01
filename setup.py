40ce358404c0a350e1895294ed9b8511efc5ed03
Samarchhabra <samarchhabra007@gmail.com>
2025-03-02 00:53:46 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Samar Chhabra',
+    author_email= 'samarchhabra007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
